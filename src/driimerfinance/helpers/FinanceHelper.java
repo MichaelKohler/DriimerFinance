28643b03481a7c18b35ef9f32e64398284996349
mrmueller <martin.u.mueller@gmail.com>
2014-11-18 20:10:37 +0100

    repair FinanceHelper :)
    repair FinanceHelper :)

---
+++ src/driimerfinance/helpers/FinanceHelper.java
@@ -0,0 +1,19 @@
+package driimerfinance.helpers;
+
+/**
+ * Useful methods for financial tasks.
+ * 
+ * (c) 2014 Driimer Finance
+*/
+public class FinanceHelper {
+	
+	/**
+     * Creates a connection to the specified host and connects to the database.
+     * 
+     * @param unformated number string
+     * @return a formatted number to be put on display for the user
+     */
+    public static String formatAmount(String unformatted) {
+        return "";
+    }
+}
