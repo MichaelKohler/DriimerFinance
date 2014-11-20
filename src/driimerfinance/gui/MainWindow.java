f7bdcf0e83e4667a6d8b53f8b04c81f50b39469c
TonyDiNozzo <info@adrian-probst.ch>
2014-11-20 22:29:49 +0100

    Mandaten in Mainwindows als Button auflisen

--- src/driimerfinance/gui/MainWindow.java
+++ src/driimerfinance/gui/MainWindow.java
@@ -1,11 +1,25 @@
 package driimerfinance.gui;
 
-import java.awt.Toolkit;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import com.sun.javafx.tk.Toolkit;
 
 import driimerfinance.helpers.GUIHelper;
+import driimerfinance.models.Mandant;
+import driimerfinance.database.DriimerDBHelper;
 
 /**
  * This is the main screen of the application.
@@ -15,13 +29,12 @@ import driimerfinance.helpers.GUIHelper;
 public class MainWindow {
 
 	JFrame frame = new JFrame("DriimerFinance");
-
+	
 	/**
 	 * Constructor
 	 */
 	public MainWindow() {
 		createGUI();
-		Toolkit.getDefaultToolkit().setDynamicLayout(true);
 	}
 
 	/**
@@ -30,8 +43,9 @@ public class MainWindow {
 	private void createGUI() {
 		GUIHelper.centerAndSizeFrame(this.frame);
 		addMenubar();
+		addForm();
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.frame.setVisible(true);
+		this.frame.setVisible(true);    
 	}
 
 	/**
@@ -40,5 +54,24 @@ public class MainWindow {
 	private void addMenubar() {
 		JMenuBar menubar = MenuBarSingleton.getMenuBarInstance();
 		this.frame.setJMenuBar(menubar);
+	}
+	
+	/**
+	 * Adds the content
+	 */
+	private void addForm() {
+		JPanel formPanel = new JPanel();
+		GridLayout layout = new GridLayout(3,4);
+		formPanel.setLayout(layout);
+		formPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		
+		DriimerDBHelper driimerdb = new DriimerDBHelper();
+		List<Mandant> mandanten = driimerdb.getAllMantanten();
+		for ( Mandant mandant : mandanten ) {
+			JButton manantButton = new JButton(mandant.getName());
+			formPanel.add(manantButton);
+			
+		this.frame.getContentPane().add(formPanel, BorderLayout.CENTER);
+		}
 	}
 }
