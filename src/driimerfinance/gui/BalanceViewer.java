1647fb7b1900dd89ec41b25f67b35f0478dbf801
Michael Kohler <mkohler@picobudget.com>
2014-11-20 22:38:10 +0100

    Window for ER and Balance

--- src/driimerfinance/gui/BalanceViewer.java
+++ src/driimerfinance/gui/BalanceViewer.java
@@ -1,18 +1,56 @@
 package driimerfinance.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import driimerfinance.database.MandantDBHelper;
+import driimerfinance.helpers.GUIHelper;
+import driimerfinance.models.Account;
 
 /**
  * Shows the Balance in a two column view.
  * 
  * (c) 2014 Driimer Finance
 */
-public class BalanceViewer extends TwoColumnsViewer {
+public class BalanceViewer {
+	JFrame frame = new JFrame("DriimerFinance - Balance");
+	
 	/**
 	 * Constructor
 	 */
     public BalanceViewer() {
-        super();
+        createGUI();
+    }
+    
+    /**
+     * Creates the GUI.
+     */
+    private void createGUI() {
+    	    GUIHelper.centerAndSizeFrame(this.frame);
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.setVisible(true);
+		
+		JPanel tablePanel = new JPanel();
+		tablePanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		
+		String[] headers = { "Aktiven", "Passiven" };
+		Object[][] data = {  }; // TODO: call getData and transform it
+		
+		JTable accountTable = new JTable(new DefaultTableModel(data, headers));
+		accountTable.setPreferredScrollableViewportSize(new Dimension(400, 300));
+		accountTable.setFillsViewportHeight(true);
+		JScrollPane scrollPane = new JScrollPane(accountTable);
+		tablePanel.add(scrollPane);
+		this.frame.getContentPane().add(tablePanel, BorderLayout.CENTER);
     }
     
     /**
@@ -20,7 +58,16 @@ public class BalanceViewer extends TwoColumnsViewer {
      * 
      * @return data for the balance
      */
-    public ArrayList<String> getData() {
-        return new ArrayList<String>();
+    private List<Account> getData() {
+        MandantDBHelper helper = new MandantDBHelper();
+        List<Account> allAccounts = helper.getAllAccounts();
+        List<Account> filteredAccounts = new ArrayList<Account>();
+        for (Account account : allAccounts) {
+        	    int typ = account.getFk_AccountType();
+        	    if (typ == 1 || typ == 2) {
+        	    	    filteredAccounts.add(account);
+        	    }
+        }
+        return filteredAccounts;
     }
 }
--- src/driimerfinance/gui/ERViewer.java
+++ src/driimerfinance/gui/ERViewer.java
@@ -1,27 +1,73 @@
 package driimerfinance.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import driimerfinance.database.MandantDBHelper;
+import driimerfinance.helpers.GUIHelper;
+import driimerfinance.models.Account;
 
 /**
  * Erfolgsrechnung overview window
  * 
  * (c) 2014 Driimer Finance
 */
-public class ERViewer extends TwoColumnsViewer {
+public class ERViewer {
+	JFrame frame = new JFrame("DriimerFinance");
 	
 	/**
 	 * Constructor
 	 */
     public ERViewer() {
-        super();
+        createGUI();
     }
     
     /**
-     * Crawls the database to get data for the Erfolgsrechnung
-     * 
-     * @return list of all data needed
+     * Creates the GUI.
      */
-    public ArrayList<String> getData() {
-        return new ArrayList<String>();
+    private void createGUI() {
+    	    GUIHelper.centerAndSizeFrame(this.frame);
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.setVisible(true);
+		
+		JPanel tablePanel = new JPanel();
+		tablePanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		
+		String[] headers = { "Aufwand", "Ertrag" };
+		Object[][] data = {  }; // TODO: call getData and transform it
+		
+		JTable accountTable = new JTable(new DefaultTableModel(data, headers));
+		accountTable.setPreferredScrollableViewportSize(new Dimension(400, 300));
+		accountTable.setFillsViewportHeight(true);
+		JScrollPane scrollPane = new JScrollPane(accountTable);
+		tablePanel.add(scrollPane);
+		this.frame.getContentPane().add(tablePanel, BorderLayout.CENTER);
+    }
+    
+    /**
+     * Crawls the database to get the data used for the ER.
+     * 
+     * @return data for the ER
+     */
+    private List<Account> getData() {
+        MandantDBHelper helper = new MandantDBHelper();
+        List<Account> allAccounts = helper.getAllAccounts();
+        List<Account> filteredAccounts = new ArrayList<Account>();
+        for (Account account : allAccounts) {
+        	    int typ = account.getFk_AccountType();
+        	    if (typ == 3 || typ == 4) {
+        	    	    filteredAccounts.add(account);
+        	    }
+        }
+        return filteredAccounts;
     }
 }
