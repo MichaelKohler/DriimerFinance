82daee9b4275f8c84122d0f5ab9403eecdd0da2f
Michael Kohler <mkohler@picobudget.com>
2014-11-27 18:55:19 +0100

    Issue #12: WIP

--- src/driimerfinance/gui/AccountPlanWindow.java
+++ src/driimerfinance/gui/AccountPlanWindow.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,6 +13,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
+
+import driimerfinance.database.MandantDBHelper;
 import driimerfinance.helpers.GUIHelper;
 import driimerfinance.models.Account;
 
@@ -18,35 +22,35 @@ import driimerfinance.models.Account;
  * Account plan overview
  * 
  * (c) 2014 Driimer Finance
-*/
+ */
 public class AccountPlanWindow {
 
 	JFrame frame = new JFrame("DriimerFinance - Kontenplan");
 	JTable accountTable = new JTable();
 	AccountPlanWindow parent = this;
-	
+
 	/**
 	 * Constructor
 	 */
-    public AccountPlanWindow() {
-        createGUI();
-    }
-    
-    /**
-     * Creates the GUI
-     */
-    private void createGUI() {
-    	    addForm();
+	public AccountPlanWindow() {
+		createGUI();
+	}
+
+	/**
+	 * Creates the GUI
+	 */
+	private void createGUI() {
+		addForm();
 		addButtons();
 		frame.setSize(400, 500);
 		GUIHelper.centerFrame(frame);
 		frame.setVisible(true);
-    }
-    
-    /**
-     * Adds the content of the window.
-     */
-    private void addForm() {
+	}
+
+	/**
+	 * Adds the content of the window.
+	 */
+	private void addForm() {
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
 		JButton addButton = new JButton("Konto hinzuf\u00fcgen");
@@ -58,13 +62,13 @@ public class AccountPlanWindow {
 			}
 		});
 		buttonPanel.add(addButton);
-		
+
 		JPanel tablePanel = new JPanel();
 		tablePanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-		
+
 		String[] headers = { "Nummer", "Name", "Typ", "Kapitalkonto" };
-		Object[][] data = {  };
-		
+		Object[][] data = getData();
+
 		accountTable = new JTable(new DefaultTableModel(data, headers));
 		accountTable.setPreferredScrollableViewportSize(new Dimension(400, 300));
 		accountTable.setFillsViewportHeight(true);
@@ -75,16 +79,28 @@ public class AccountPlanWindow {
 		frame.getContentPane().add(tablePanel, BorderLayout.CENTER);
 	}
 
-    /**
-     * Adds the button on the bottom of the frame.
-     */
+	/**
+	 * Gets the data for the view once the window is started
+	 */
+	private Object[][] getData() {
+		MandantDBHelper helper = new MandantDBHelper();
+		List<Account> allAccounts = helper.getAllAccounts();
+		Object[][] rows = new Object[allAccounts.size()][4];
+		
+		return rows;
+	}
+
+	/**
+	 * Adds the button on the bottom of the frame.
+	 */
 	private void addButtons() {
 		JPanel buttonPanel = new JPanel();
 		JButton okButton = new JButton("OK");
 		okButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// we don't need to save this since an account plan is only a virtual entity
+				// we don't need to save this since an account plan is only a
+				// virtual entity
 				frame.dispose();
 			}
 		});
@@ -92,15 +108,17 @@ public class AccountPlanWindow {
 		frame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
 		this.frame.getRootPane().setDefaultButton(okButton);
 	}
-	
+
 	/**
 	 * Adds the new account to the account table.
 	 * 
-	 * @param acc to add to the table
+	 * @param acc
+	 *            to add to the table
 	 */
 	public void addAccountToTable(Account acc) {
 		DefaultTableModel model = (DefaultTableModel) (accountTable.getModel());
-		Object[] newRow = { acc.getId().toString(), acc.getName(), acc.getFk_AccountType() };
+		Object[] newRow = { acc.getId().toString(), acc.getName(),
+				acc.getFk_AccountType() };
 		model.addRow(newRow);
 		frame.repaint();
 	}
