6f4e7114abd9e2f589ac0ee355f64a5f69b84b33
TonyDiNozzo <info@adrian-probst.ch>
2014-12-07 18:10:01 +0100

    Adding Set Balance for Jahresabschluss

--- src/driimerfinance/models/AnnualAccounts.java
+++ src/driimerfinance/models/AnnualAccounts.java
@@ -1,5 +1,10 @@
 package driimerfinance.models;
 
+
+import java.util.List;
+
+import driimerfinance.database.MandantDBHelper;
+
 /**
  * Symbolic class to do the yearly annual accounts step.
  * 
@@ -19,4 +24,35 @@ public class AnnualAccounts {
     public void doAnnualAccounts() {
         
     }
+    
+    /**
+     * Set balance for all the spending positions to 0.
+     */
+    public void clearSpendingPositions() {
+    	    MandantDBHelper helper = new MandantDBHelper();
+        List<Account> allAccounts = helper.getAllAccounts();
+        for (Account account : allAccounts) {
+        	    int typ = account.getFk_AccountType();
+        	    if (typ == 4) { // Typ: Aufwand
+        	    	   account.setBalance(0);
+        	    }
+        }
+    }
+    
+    /**
+     * Set balance for all the earning positions to 0.
+     */
+    public void clearEarningPositions() {
+        MandantDBHelper helper = new MandantDBHelper();
+        List<Account> allAccounts = helper.getAllAccounts();
+        for (Account account : allAccounts) {
+        	    int typ = account.getFk_AccountType();
+        	    if (typ == 3) { // Typ: Ertrag
+        	    	    account.setBalance(0);
+        	    }
+        }
+    }
+    
+    
+    
 }
