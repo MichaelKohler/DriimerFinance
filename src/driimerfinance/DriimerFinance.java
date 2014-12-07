695dee987d7ce61661529c2842a546eea482748b
Michael Kohler <mkohler@picobudget.com>
2014-12-07 16:08:10 +0100

    re enter key if necessary

--- src/driimerfinance/DriimerFinance.java
+++ src/driimerfinance/DriimerFinance.java
@@ -11,6 +11,7 @@ import java.util.Properties;
 import javax.swing.JOptionPane;
 
 import driimerfinance.database.DriimerDBHelper;
+import driimerfinance.gui.EnterLicenseKeyWindow;
 import driimerfinance.gui.MainWindow;
 import driimerfinance.gui.LoginWindow;
 import driimerfinance.gui.MainWindowSingleton;
@@ -19,75 +20,79 @@ import driimerfinance.helpers.FinanceHelper;
 import driimerfinance.models.Mandant;
 import driimerfinance.models.User;
 
-
 /**
  * Main entry point class
  * 
  * (c) 2014 Driimer Finance
-*/
+ */
 public class DriimerFinance {
-	
-	//random comment
 
-    /**
-     * The main method starts everything. This includes initializing settings
-     * and starting the Main graphical screen.
-     * 
-     * @param args the command line arguments
-     * @throws Exception this is only used for the test code -- to be removed
-     */
-    public static void main(String[] args) throws Exception {
-    		Properties properties = new Properties();
-    		String licenseKey = "";
-    		try {
-    			properties.load(new FileInputStream(new File(
-    					"bin/driimerfinance/database/database.properties")
-    					.getAbsoluteFile()));
-    			licenseKey = properties.getProperty("licensekey");
-    		} catch (IOException e) {
-    			e.printStackTrace();
-    		}
-    		
-    		if (licenseKey != null && licenseKey.equals("") == false) {
-    			// existing key, checking validity
-    			boolean validity = FinanceHelper.checkLicense(licenseKey);
-    			if (validity) {
-    				//new LoginWindow(); // TODO: enable for production
-        			MainWindowSingleton.getMainWindowInstance(); // TODO: remove for production
-    			}
-    			else {
-    				JOptionPane.showMessageDialog(null, "Der Lizenzschlüssel ist nicht gültig!", "Fehler", JOptionPane.ERROR_MESSAGE);
-    			}
-    		}
-    		else {
-    			new SetupWindow();
-    		}
+	// random comment
 
-    		
-    		
-    		
-    		
-    		// TEST CODE BELOW
-        DriimerDBHelper driimerdb = new DriimerDBHelper();
-                
-        System.out.println("Listing all Users: ");
-        List<User> users = driimerdb.getAllUsers();
-        for(User user : users) {
-        	System.out.println("ID: " + user.getId() + " Name: " + user.getName() + " Vorname: " + user.getVorname() + " username: " + user.getUsername() + " Password: " + user.getPassword());
-        }
-        //Mandant testmandant = new Mandant();
-        //testmandant.setName("stampfli");
-        //testmandant.setDBSchema("stampfli");
-        //testmandant.createInDB();
-        //driimerdb.addMandant(testmandant);
-        
-        
-        System.out.println("Listing all Clients: ");
-        List<Mandant> mandanten = driimerdb.getAllMantanten();
-        for(Mandant mandant : mandanten) {
-        	System.out.println("ID: " + mandant.getId() + " Name: " + mandant.getName() + " DBSchema: " + mandant.getDBSchema());
-        }
-        driimerdb.closeConnection();
-     }
-    
+	/**
+	 * The main method starts everything. This includes initializing settings
+	 * and starting the Main graphical screen.
+	 * 
+	 * @param args
+	 *            the command line arguments
+	 * @throws Exception
+	 *             this is only used for the test code -- to be removed
+	 */
+	public static void main(String[] args) throws Exception {
+		Properties properties = new Properties();
+		String licenseKey = "";
+		try {
+			properties.load(new FileInputStream(new File(
+					"bin/driimerfinance/database/database.properties")
+					.getAbsoluteFile()));
+			licenseKey = properties.getProperty("licensekey");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		if (licenseKey != null && licenseKey.equals("") == false) {
+			// existing key, checking validity
+			boolean validity = FinanceHelper.checkLicense(licenseKey);
+			if (validity) {
+				// new LoginWindow(); // TODO: enable for production
+				MainWindowSingleton.getMainWindowInstance(); // TODO: remove for
+																// production
+			} else {
+				JOptionPane.showMessageDialog(null,
+						"Der Lizenzschlüssel ist nicht gültig!", "Fehler",
+						JOptionPane.ERROR_MESSAGE);
+				new EnterLicenseKeyWindow();
+			}
+		} else {
+			new SetupWindow();
+		}
+
+		// TEST CODE BELOW
+		DriimerDBHelper driimerdb = new DriimerDBHelper();
+
+		System.out.println("Listing all Users: ");
+		List<User> users = driimerdb.getAllUsers();
+		for (User user : users) {
+			System.out.println("ID: " + user.getId() + " Name: "
+					+ user.getName() + " Vorname: " + user.getVorname()
+					+ " username: " + user.getUsername() + " Password: "
+					+ user.getPassword());
+		}
+		// Mandant testmandant = new Mandant();
+		// testmandant.setName("stampfli");
+		// testmandant.setDBSchema("stampfli");
+		// testmandant.createInDB();
+		// driimerdb.addMandant(testmandant);
+
+		System.out.println("Listing all Clients: ");
+		List<Mandant> mandanten = driimerdb.getAllMantanten();
+		for (Mandant mandant : mandanten) {
+			System.out
+					.println("ID: " + mandant.getId() + " Name: "
+							+ mandant.getName() + " DBSchema: "
+							+ mandant.getDBSchema());
+		}
+		driimerdb.closeConnection();
+	}
+
 }
---
+++ src/driimerfinance/gui/EnterLicenseKeyWindow.java
@@ -0,0 +1,112 @@
+package driimerfinance.gui;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Properties;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import driimerfinance.helpers.FinanceHelper;
+import driimerfinance.helpers.GUIHelper;
+
+/**
+ * Used at the beginning to install the software.
+ * 
+ * (c) 2014 Driimer Finance
+*/
+public class EnterLicenseKeyWindow {
+	
+	ImageIcon icon = new ImageIcon("images/DF.png");
+	JFrame frame = new JFrame("DriimerFinance - Lizenz");
+	JTextField licenseField = new JTextField();
+	
+	/**
+	 * Constructor
+	 */
+    public EnterLicenseKeyWindow() {
+        createGUI();
+    }
+    
+    /**
+     * Creates the GUI
+     */
+    private void createGUI() {
+		addForm();
+		addButtons();
+		this.frame.setSize(400, 100);
+		GUIHelper.centerFrame(this.frame);
+		this.frame.setIconImage(icon.getImage());
+		this.frame.setVisible(true);
+	}
+    
+    /**
+	 * Adds the form
+	 */
+    private void addForm() {
+		JPanel formPanel = new JPanel();
+		GridLayout layout = new GridLayout(1, 2);
+		formPanel.setLayout(layout);
+		formPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		JLabel licenseLabel = new JLabel("Lizenzschlüssel");
+		formPanel.add(licenseLabel);
+		formPanel.add(licenseField);
+		this.frame.getContentPane().add(formPanel, BorderLayout.CENTER);
+    }
+    
+    /**
+     * Adds the buttons on the bottom of the frame.
+     */
+	private void addButtons() {
+		JPanel buttonPanel = new JPanel();
+		JButton okButton = new JButton("OK");
+		okButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				boolean licenseOK = FinanceHelper.checkLicense(licenseField.getText());
+				if (!licenseOK) {
+					JOptionPane.showMessageDialog(frame, "Der Lizenzschlüssel ist nicht gültig!", "Fehler", JOptionPane.ERROR_MESSAGE);
+				}
+				else {
+					Properties prop = new Properties();
+					try {
+						URL url = getClass().getResource("../../driimerfinance/database/database.properties");
+						prop.load(new FileInputStream(url.toURI().getPath()));
+						prop.setProperty("licensekey", licenseField.getText());
+						prop.store(new FileOutputStream(url.toURI().getPath()), null);
+					} catch (IOException ex) {
+						ex.printStackTrace();
+					} catch (URISyntaxException ex) {
+						ex.printStackTrace();
+					}
+					frame.dispose();
+					MainWindowSingleton.getMainWindowInstance();
+				}
+			}
+		});
+		JButton cancelButton = new JButton("Abbrechen");
+		cancelButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+		buttonPanel.add(okButton, BorderLayout.WEST);
+		buttonPanel.add(cancelButton, BorderLayout.EAST);
+		this.frame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+		this.frame.getRootPane().setDefaultButton(okButton);
+	}
+}
