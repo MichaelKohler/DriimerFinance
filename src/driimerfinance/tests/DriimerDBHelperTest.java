6ccfb6561dad415c12ddb8d0a3e3365659eac71f
Michael Kohler <mkohler@picobudget.com>
2014-12-09 08:35:39 +0100

    a second, but failing test (commented out)

--- src/driimerfinance/tests/DriimerDBHelperTest.java
+++ src/driimerfinance/tests/DriimerDBHelperTest.java
@@ -8,19 +8,18 @@ import com.mockrunner.mock.jdbc.MockConnection;
 import com.mockrunner.mock.jdbc.MockResultSet;
 
 import driimerfinance.database.DriimerDBHelper;
+import driimerfinance.models.User;
 
 public class DriimerDBHelperTest extends BasicJDBCTestCaseAdapter {
-	private void prepareEmptyResultSet() {
+
+	public void testGetAllUsers() throws SQLException {
 		MockConnection connection = getJDBCMockObjectFactory()
 				.getMockConnection();
 		StatementResultSetHandler statementHandler = connection
 				.getStatementResultSetHandler();
 		MockResultSet result = statementHandler.createResultSet();
 		statementHandler.prepareGlobalResultSet(result);
-	}
-
-	public void testGetAllUsers() throws SQLException {
-		prepareEmptyResultSet();
+		
 		DriimerDBHelper helper = new DriimerDBHelper();
 		helper.getAllUsers();
 		verifySQLStatementExecuted("select * from user");
@@ -28,4 +27,25 @@ public class DriimerDBHelperTest extends BasicJDBCTestCaseAdapter {
         verifyAllResultSetsClosed();
         verifyAllStatementsClosed();
 	}
+	
+	/*public void testGetUserById() throws SQLException {
+		MockConnection connection = getJDBCMockObjectFactory()
+				.getMockConnection();
+		StatementResultSetHandler statementHandler = connection
+				.getStatementResultSetHandler();
+		MockResultSet result = statementHandler.createResultSet();
+		result.addColumn("idUser", new Integer[]{1});
+		result.addColumn("Name", new String[]{"Foo"});
+		result.addColumn("Vorname", new String[]{"Foo"});
+		result.addColumn("Password", new String[]{"Foo"});
+		result.addColumn("username", new String[]{"Foo"});
+		statementHandler.prepareGlobalResultSet(result);
+		
+		DriimerDBHelper helper = new DriimerDBHelper();
+		helper.getUserById(1);
+		verifySQLStatementExecuted("select * from user where idUser");
+        verifyNotRolledBack();
+        verifyAllResultSetsClosed();
+        verifyAllStatementsClosed();
+	}*/
 }
